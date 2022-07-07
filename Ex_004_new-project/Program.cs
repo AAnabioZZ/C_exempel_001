a48de531590f614700daf8310fb6405631b14e07
Ars <ars9000@gmail.com>
2022-07-07 18:11:41 +0300

    деление случайного числа от 1 до 10 на другое

--- Ex_004_new-project/Program.cs
+++ Ex_004_new-project/Program.cs
@@ -1,3 +1,7 @@
-﻿double a = 46;
-double b = 12;
+﻿double a = new Random().Next(1,11);
+double b = new Random().Next(1,11);
+Console.Write(a);
+Console.Write("/");
+Console.Write(b);
+Console.Write("=");
 Console.WriteLine(a / b);
