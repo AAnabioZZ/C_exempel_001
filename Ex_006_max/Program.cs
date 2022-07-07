c6d0b61a38af4fbef70be2363cdf6a50842ce641
Ars <ars9000@gmail.com>
2022-07-07 18:58:18 +0300

    max

---
+++ Ex_006_max/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 2;
+int b = 1;
+int c = 4;
+int d = 8;
+int e = 1;
+int f = 9;
+int max = 0;
+if(a>max) max=a;
+if(b>max) max=b;
+if(c>max) max=c;
+if(d>max) max=d;
+if(e>max) max=e;
+if(f>max) max=f;
+Console.WriteLine(max);
