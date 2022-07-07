b8ce441181f5bdbbb01e0c161c41cda18905cfd4
Ars <ars9000@gmail.com>
2022-07-07 18:35:21 +0300

    if_else_ToLower

---
+++ Ex_005_if_else/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Кто там? Тут совсем темно, представьтесь пожалуйста.");
+string username = Console.ReadLine();
+if (username.ToLower() == "марьям")
+{
+    Console.WriteLine("Ура ! Наконецто ты пришла, я так рад тебе!");
+} else {
+    Console.Write("Чтож, ");
+    Console.Write(username);
+    Console.WriteLine(" проходи.");
+}
